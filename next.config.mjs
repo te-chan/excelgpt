0575a81426777a2ae5a13ffbe91f2cb18fda54f3
te-chan <50950268+te-chan@users.noreply.github.com>
2024-10-27 22:16:04 +0900

    Refactor next.config.mjs to remove unused properties

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    distDir: 'docs',
-    basePath: '/excelgpt',
-    assetPrefix: '/excelgpt/',
 };
 
 export default nextConfig;
