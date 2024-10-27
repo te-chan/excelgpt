8c70633c64242ae93ca5a3aa612b854b91d08d45
te-chan <50950268+te-chan@users.noreply.github.com>
2024-10-27 21:53:01 +0900

    Refactor next.config.mjs to add basePath and assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    distDir: 'docs'
+    distDir: 'docs',
+    basePath: '/excelgpt',
+    assetPrefix: '/excelgpt/',
 };
 
 export default nextConfig;
